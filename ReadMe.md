68a61edaee6427ec057f55a6549f9e507b5bfe21
Prince Gupta <prince.guptar27@gmail.com>
2024-09-23 23:14:40 +0530

    initial commit

---
+++ ReadMe.md
@@ -0,0 +1,6 @@
+# Project Setup
+## Required Python Version 3.1 or higher with pip
+- Install required dependencies using `pip install -r requirements.txt`
+- To run application using inputs from config file `py census.py FILE` 
+- To run application by entering url on command prompt `py census.py`
+
---
+++ census.py
@@ -0,0 +1,34 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+import configparser
+import sys
+
+
+if len(sys.argv) > 1 and sys.argv[1] == "FILE":
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    url = config['input']['url']
+else:
+    url = input("Enter website URL: ")
+
+#url="https://www.census2011.co.in/states.php"
+response=requests.get(url)
+soup=BeautifulSoup(response.content,'html.parser')
+data = []
+table = soup.find("table")
+
+#table header
+table_headers=table.find_all('th')
+data.append([th.text for th in table_headers])
+
+#table row
+table_rows=table.find_all('tr')
+for tr in table_rows:
+    td=tr.find_all('td')
+    if(len(td)>0):
+        row=[tr.text for tr in td]
+        data.append(row)
+
+df=pd.DataFrame(data)
+df.to_csv('file1.csv')
