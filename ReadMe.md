f3e6622a9c942986db0c048146c9472070a4fd2e
Prince Gupta <prince.guptar27@gmail.com>
2024-09-24 19:31:38 +0530

    added comments

--- ReadMe.md
+++ ReadMe.md
@@ -1,5 +1,9 @@
-# Project Setup
-## Required Python Version 3.1 or higher with pip
+# Census 2011 Data - Web Scraping
+The Government of India conducts periodical census (every 10 years) which becomes an important dataset for doing socio-economic analysis and aid in evidence-based policymaking. The last census was conducted in 2011 and the 2021 census was postponed due to Covid-19 pandemic. 
+This project scrapes data from https://www.census2011.co.in/ and exports the obtained data in a csv file which can be used to perform socioeconomic analysis. 
+
+## Project Setup
+### Required Python Version 3.1 or higher with pip
 - Install required dependencies using `pip install -r requirements.txt`
 - To run application using inputs from config file `py census.py FILE` 
 - To run application by entering url on command prompt `py census.py`
--- census.py
+++ census.py
@@ -1,34 +1,36 @@
-import requests
-import pandas as pd
-from bs4 import BeautifulSoup
-import configparser
+# Importing necessary libraries 
 import sys
+import configparser
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
 
-
-if len(sys.argv) > 1 and sys.argv[1] == "FILE":
+# Creating option of either taking url input from config file or taking input from user in terminal 
+if len(sys.argv) > 1 and sys.argv[1] == "FILE": # If user types py census.py FILE in terminal, input from config file is taken.   
     config = configparser.ConfigParser()
     config.read('config.ini')
     url = config['input']['url']
 else:
-    url = input("Enter website URL: ")
+    url = input("Enter website URL: ")  # User is asked to enter the url "https://www.census2011.co.in/states.php"
 
-#url="https://www.census2011.co.in/states.php"
+# Using Beautiful Soup to web scrape census data  
 response=requests.get(url)
 soup=BeautifulSoup(response.content,'html.parser')
-data = []
-table = soup.find("table")
+data = [] 
+table = soup.find("table") #finding the required table on the webpage
 
-#table header
+# scraping table header
 table_headers=table.find_all('th')
 data.append([th.text for th in table_headers])
 
-#table row
+# scraping data in table rows
 table_rows=table.find_all('tr')
-for tr in table_rows:
+for tr in table_rows: # for loop is used to scrape data from each row 
     td=tr.find_all('td')
     if(len(td)>0):
         row=[tr.text for tr in td]
         data.append(row)
 
+# Creating dataframe to store data and exporting it into a csv file. 
 df=pd.DataFrame(data)
 df.to_csv('census_data.csv')
